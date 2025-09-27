f371150dfabdfc406ec0f609418bc8291961399c
Kamit <timaplay137@gmail.com>
2025-09-27 09:37:31 +0300

    минималка

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	pg "anima/sql"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -8,7 +9,7 @@ import (
 )
 
 type UserRegLog struct {
-	Username string `json:"username"`
+	Email    string `json:"username"`
 	Password string `json:"password"`
 }
 
@@ -31,14 +32,80 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, fmt.Sprintf("Error unmarshalling body: %v", err), http.StatusBadRequest)
 			return
 		}
-
+		retValue := pg.Login(userReq.Email, userReq.Password)
+		if retValue == "bad password" {
+			w.WriteHeader(http.StatusUnauthorized)
+		} else if retValue == "ok" {
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"status":  "success",
+				"message": "Пользователь вошёл",
+				"email":   userReq.Email,
+			})
+		}
 	}
 }
 func registerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error reading body: %v", err), http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+		var userReq UserRegLog
+		err = json.Unmarshal(body, &userReq)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error unmarshalling body: %v", err), http.StatusBadRequest)
+			return
+		}
+		retValue := pg.Reg(userReq.Email, userReq.Password)
+		if retValue == "email already in db" {
+			w.WriteHeader(http.StatusUnauthorized)
+		} else if retValue == "ok" {
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"status":  "success",
+				"message": "Пользователь зарегистрирован",
+				"email":   userReq.Email, // ← возвращаем email
+			})
+		}
+	}
 }
-func homeHandler(w http.ResponseWriter, r *http.Request) {
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		body, err := io.ReadAll(r.Body)
+
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error reading body: %v", err), http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+		var userReq UserRegLog
+		err = json.Unmarshal(body, &userReq)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error unmarshalling body: %v", err), http.StatusBadRequest)
+			return
+		}
+		save := pg.ReadJsonDb(userReq.Email)
+		if save == "" {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"status": "success",
+				"save":   save,
+			})
+		}
+	}
 }
 func adminpanelHandler(w http.ResponseWriter, r *http.Request) {
 
